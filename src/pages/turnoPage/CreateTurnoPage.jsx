b0eafa90b9804cbff883f0a6635aaa67201630ff
juanmacruzponce <cruzponce.juan@gmail.com>
2024-12-27 11:27:33 -0300

    changes

--- src/pages/turnoPage/CreateTurnoPage.jsx
+++ src/pages/turnoPage/CreateTurnoPage.jsx
@@ -1,58 +1,68 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { RouterContext } from "../../context/UseContext"
 import { useFetch } from "../../hooks/useFetch"
 
 export const CreateTurnoPage = ({endpoint}) => {
     const {routerData} = useContext(RouterContext)
     const {data, fetchData, isLoading, error} = useFetch()
-    const [nroAcceso, setNroAcceso] = useState()
-    const [matricula, setMatricula] = useState()
-    const [descripcion, setDescripcion] = useState()
-    const [fechaInicio, setFechaInicio] = useState()
-    const [fechaFirmado, setFechaFirmado] = useState()
-    const [estado, setEstado] = useState()
+    const [dni, setDni] = useState()
+    const [especialidad, setEspecialidad] = useState([])
+    const [idEspecialidad, setIdEspecialidad] = useState("")
     const [validate, setValidate] = useState(true)
 
-    const handleChange = (e) =>{
-        setValidate(true)
-        const{name, value}= e.target
-        if(name === 'nroAcceso') setNroAcceso(parseInt(value))
-        if(name === 'matricula') setMatricula(parseInt(value))
-        if(name === 'descripcion') setDescripcion(value)
-        if(name === 'fechaInicio') setFechaInicio(value)
-        if(name === 'fechaFirmado') setFechaFirmado(value)
-        if(name === 'estado') setEstado(value)   
-    }
+    const handleChangeEspecialidad = (e) => {
+        setValidate(true);
+        const { value } = e.target;
+        setIdEspecialidad(value);
+      };
+    
+    const handleChangeDni = (e) => {
+        setValidate(true);
+        setDni(e.target.value); // Actualiza el estado con el DNI ingresado
+    };      
+
+    useEffect(() => {
+        const loadEspecialidades = async () => {
+          try {
+            const response = await fetch(`http://localhost:3000/especialidad`)
+            const result = await response.json();
+            const especialidadesArray = Object.values(result)
+            setEspecialidad(especialidadesArray);
+          } catch (err) {
+            console.error("Error al cargar especialidades:", err)
+          }
+        }
+        loadEspecialidades()
+      }, []);
+      
+
+
     const handleSubmit = (e)=>{
         e.preventDefault()
-        if(isNaN(nroAcceso) || isNaN(matricula) || descripcion == '' || fechaInicio == '' || fechaFirmado == '' || estado == ''){
+        if(isNaN(dni) || isNaN(matricula) || fecha == '' || hora == ''){
             setValidate(false)
             return
         }
-        fetchData(`http://localhost:3000${endpoint}`,'POST',{nroAcceso,matricula,descripcion,fechaInicio,fechaFirmado,estado})
+        fetchData(`http://localhost:3000${endpoint}`,'POST',{dni,matricula,fecha,hora})
     }
     return(
-        <>
-            {
-                routerData && (
-                    <div className="form-ById">
-                        <form onSubmit={handleSubmit}>
-                            {routerData.items &&  
-                                    routerData.items.map((item, index) => (
-                                        item.type === 'date' ? <input key={index} name={item.name} type="date" placeholder={item.name} onChange={handleChange}/> : 
-                                            item.type === 'text' ? <input key={index} name={item.name} type="text" placeholder={item.name} onChange={handleChange}/> :
-                                                <input key={index} name={item.name} type="number" placeholder={item.name} onChange={handleChange}/>
-                                            
-                                        )
-                                    )
-                            }
-                            <label>
-                                <button type="submit">Enviar</button>
-                            </label>   
-                        </form>
-                    </div>
-                    )
-            }   
+        <>  
+            <form onSubmit={handleSubmit}>
+            <input type="number" name="dni" placeholder = "DNI" value={parseInt(dni)} onChange={handleChangeDni} required />
+            <br />
+                <select name="idEspecialidad" id="especialidad" value={idEspecialidad} onChange={handleChangeEspecialidad}>
+                <option value="" disabled> {"Seleccione una especialidad"} </option>
+                {especialidad.map((especialidad) => (
+                    <option key={especialidad.idEspecialidad} value={especialidad.idEspecialidad}>
+                    {especialidad.nombre}
+                    </option>
+                ))}
+                </select>
+            <br /> 
+
+                    <button type="submit">Generar Turno</button>
+            </form>
+
             {!isLoading && 
                 data ? data.message : <>{error}</>}
             {!validate && <p className="error">Los campos son requeridos</p>}
