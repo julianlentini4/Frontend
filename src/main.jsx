76c9d906a9aa4f06438af76fe7bf9c63249929be
juanmacruzponce <cruzponce.juan@gmail.com>
2025-01-17 09:52:54 -0300

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { App } from './pages/App'
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <React.StrictMode>
+    <div><Toaster/></div>
       <App/>
     </React.StrictMode>
   </BrowserRouter>
